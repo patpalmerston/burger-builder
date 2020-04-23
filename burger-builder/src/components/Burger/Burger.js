94a99aeb75547d573c8fabc8bdd5377bf3066d44
Pat Palmerston <patpalmerston@gmail.com>
2020-04-23 16:45:38 -0700

    make burger ingredients dynamic

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,11 +4,22 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const Burger = (props) => {
+    // need to change the props object into an Array
+    // Then also duplicate as many of the keys as they have number values('cheese: 2') = two cheeses
+    const transformedIngredients = Object.keys(props.ingredients).map(
+        (igKey) => {
+            //return a copy, array that holds as many keys given by props
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                // creating a unique key for each duplicate ingredient
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        }
+    );
+    console.log(transformedIngredients);
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Auxillary';
 import Burger from '../../components/Burger/Burger';
 
+// need to add status so that we can change the state depending on the ingredients selected, which we can then pass to the burger component
 class BurgerBuilder extends Component {
+    // below is legacy state constructor
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+
+    //     }
+    // }
+    // moder state constructor
+    // start with hard coding a state object that has needed ingredients
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2,
+        },
+    };
     render() {
         return (
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </Aux>
         );
