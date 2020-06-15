82cce1f17627e24ced778d2a10ad841cab337a26
Pat Palmerston <patpalmerston@gmail.com>
2020-06-15 13:56:44 -0700

    mobile logo

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -9,7 +9,9 @@ import classes from '../SideDrawer/SideDrawer.module.css';
 const SideDrawer = () => {
     return (
         <div className={classes.SideDrawer}>
-            <Logo />
+            <div className={classes.Logo}>
+                <Logo />
+            </div>
             <nav>
                 <NavigationItems />
             </nav>
