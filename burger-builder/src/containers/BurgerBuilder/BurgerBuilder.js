0dfb8fdec132909132915eff95b5d6aad1d7154a
Pat Palmerston <patpalmerston@gmail.com>
2020-06-23 15:24:40 -0700

    error handling

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 // this is a global variable so we call it outside of the component, global variable are usually all uppercase
 const INGREDIENT_PRICES = {
@@ -188,4 +189,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
