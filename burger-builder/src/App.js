5f97650301b6ab23767e58485b62b8d587df9728
Pat Palmerston <patpalmerston@gmail.com>
2020-07-22 15:31:18 -0700

    routes added for components

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -8,8 +9,11 @@ class App extends React.Component {
         return (
             <div>
                 <Layout>
-                    <BurgerBuilder />
-                    <Checkout />
+                    {/* with out the switch we would use exact to get the home page, but with switch it give us one hit at a time, but homepage needs to be last as if it hits the slash prefix it stops searching for the path */}
+                    <Switch>
+                        <Route path='/checkout' component={Checkout} />
+                        <Route path='/' component={BurgerBuilder} />
+                    </Switch>
                 </Layout>
             </div>
         );
