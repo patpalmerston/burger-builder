83536a442a51016f05fd5096be925b2688ee558b
Pat Palmerston <patpalmerston@gmail.com>
2020-08-20 19:51:49 -0700

    notes

--- burger-builder/src/containers/CheckOut/CheckOut.js
+++ burger-builder/src/containers/CheckOut/CheckOut.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
-
+// notes
 class Checkout extends Component {
     state = {
         ingredients: {
